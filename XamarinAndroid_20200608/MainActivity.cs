47cd8c2a9b1a86cd8e80b1c26c5d16d70ea9146e
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2020-06-08 17:15:02 +0200

    Add project files.

---
+++ XamarinAndroid_20200608/MainActivity.cs
@@ -0,0 +1,55 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Android.Content;
+
+namespace XamarinAndroid_20200608
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public EditText Etxt_Eingabe { get; set; }
+        public Button Btn_KlickMich { get; set; }
+        public Button Btn_Impliziet { get; set; }
+        public Button Btn_Expliziet { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            Etxt_Eingabe = FindViewById<Android.Widget.EditText>(Resource.Id.main_etxt_Eingabe);
+            Btn_KlickMich = FindViewById<Button>(Resource.Id.main_btn_KlickMich);
+
+            Btn_KlickMich.Click += (s, e) => Toast.MakeText(this, $"Deine gewählte Zahl ist {Etxt_Eingabe.Text}.", ToastLength.Short).Show();
+            //Btn_KlickMich.Click += ButtonClickEvent;
+
+            //Implizieter Intent
+            Btn_Impliziet = FindViewById<Button>(Resource.Id.main_btn_ImplIntent);
+            Intent implIntent = new Intent(Intent.ActionView, Android.Net.Uri.Parse("http://www.google.de"));
+            Btn_Impliziet.Click += (s, e) => StartActivity(implIntent);
+
+            //Explizieter Intent
+            Btn_Expliziet = FindViewById<Button>(Resource.Id.main_btn_ExplIntent);
+            Intent explIntent = new Intent(this, typeof(ShowPictureActivity));
+            Btn_Expliziet.Click += (s, e) => StartActivity(explIntent); 
+        }
+
+        private void ButtonClickEvent(object s, EventArgs e)
+        {
+            Toast.MakeText(this, $"Deine gewählte Zahl ist {Etxt_Eingabe.Text}.", ToastLength.Short).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ XamarinAndroid_20200608/ShowPictureActivity.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinAndroid_20200608
+{
+    [Activity(Label = "ShowPictureActivity")]
+    public class ShowPictureActivity : Activity
+    {
+        public EditText ETxt_Width { get; set; }
+        public EditText ETxt_Height { get; set; }
+        public Button Btn_LadeBild { get; set; }
+        public ImageView ImgV_RandomPic { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.activity_showpicture);
+
+            ETxt_Height = FindViewById<EditText>(Resource.Id.etx_randomPic_height);
+            ETxt_Width = FindViewById<EditText>(Resource.Id.etx_randomPic_width);
+            Btn_LadeBild = FindViewById<Button>(Resource.Id.btn_goRandomPic);
+            ImgV_RandomPic = FindViewById<ImageView>(Resource.Id.imgRandomPic);
+
+            Btn_LadeBild.Click += async (s, e) =>
+            {
+                int breite = int.Parse(ETxt_Width.Text);
+                int hoehe = int.Parse(ETxt_Height.Text);
+
+                ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.SetMessage("Downloading Picture...");
+
+                using (WebClient client = new WebClient())
+                {
+                    progressDialog.Show();
+
+                    byte[] bild = await client.DownloadDataTaskAsync($"https://placeimg.com/{breite}/{hoehe}/any");
+                    Bitmap bitmap = BitmapFactory.DecodeByteArray(bild, 0, bild.Length);
+
+                    ImgV_RandomPic.SetImageBitmap(bitmap);
+
+                    progressDialog.Dismiss();
+                }
+            };
+        }
+    }
+}
